4fa0e0459e62b9e4989c97f09513557ab7a1217c
Vlada Goreva <mail@vgoreva.ru>
2023-02-25 15:47:51 +0300

    Третья попытка

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,30 @@
 let openButtonEditForm = document.querySelector('.profile__edit-button')
-let popupDisplayClass = document.querySelector('.popup');
+let popup = document.querySelector('.popup');
 let userNameElement = document.querySelector('.profile__name');
 let userDetailsElement = document.querySelector('.profile__details');
 let editForm = document.querySelector('.popup__form');
-let userNameEditForm = document.querySelector('.popup__input_name');
-let userDetailsEditForm = document.querySelector('.popup__input_details');
-let saveButtonEditForm = document.querySelector('.popup__save-button');
+let userNameEditForm = document.querySelector('.popup__input_type_name');
+let userDetailsEditForm = document.querySelector('.popup__input_type_details');
 let closeButtonEditForm = document.querySelector('.popup__close-button');
 
-openButtonEditForm.onclick = function openEditForm() {
-    popupDisplayClass.classList.add('popup_opened');
+openButtonEditForm.addEventListener('click', function openEditForm() {
+    popup.classList.add('popup_opened');
     userNameEditForm.value = userNameElement.textContent;
     userDetailsEditForm.value = userDetailsElement.textContent;
 }
+)
 
-closeButtonEditForm.onclick = function closeEditForm() {
-    popupDisplayClass.classList.remove('popup_opened');
+function closeEditForm() {
+    popup.classList.remove('popup_opened');
 }
 
-function handleFormSubmit (evt) {
-    evt.preventDefault(); 
-    userNameEditForm.value = userNameElement.textContent;
-    userDetailsEditForm.value = userDetailsElement.textContent;
-}
+closeButtonEditForm.addEventListener('click', closeEditForm);
 
-saveButtonEditForm.addEventListener('submit', handleFormSubmit); 
+function handleFormSubmit(event) {
+        event.preventDefault();
+        userNameElement.textContent = userNameEditForm.value;
+        userDetailsElement.textContent= userDetailsEditForm.value;
+        closeEditForm();
+      }
+
+editForm.addEventListener('submit',handleFormSubmit)
